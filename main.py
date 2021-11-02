0a5c6b2b15edb3c49048d27572c853d8febf1ace
Romaine Allen <romallen1@gmail.com>
2021-11-02 15:43:27 +0900

    Retrive data from website

--- main.py
+++ main.py
@@ -1,53 +1,78 @@
 import requests
 from bs4 import BeautifulSoup
-import mechanize
-import http.cookiejar
 import json
+from dotenv import load_dotenv
+import os
+from requests.sessions import Request
+
+load_dotenv()
+
+session = requests.Session()
 
 cookies = {
-    "XSRF-TOKEN": "eyJpdiI6IndQUjk1bWd6Z2NNZnM5ZTJVWlozQ2c9PSIsInZhbHVlIjoiMmhoSWVIdmd5NDhiMlhSanlDTmU5RUhnZ1BCRlVUdDlYVDBCanpWalovbHgxVnF2TlR4aWxKNFQzOStxTGVmK0VNcWtEaGNJMllqWGV1VG81SnR2VE1hZk9Gc2YxUVBGNXdvWDZoWm5IcHVPWVM0WWlwcnJkbUROMDBoNWtpb1UiLCJtYWMiOiJlYzRiNDlmYWJkM2U4ODgxNzUxODEwYjlhMDI1YTk4MjhlZWYxZDhjMTBjZWNjM2M1Y2Y2YWVkZWE1NWRhZTYxIiwidGFnIjoiIn0%3D",
-    "mymoneyja_session": "eyJpdiI6ImFhTjZGOTU1eHI2VTQxNlZ6bDFIUUE9PSIsInZhbHVlIjoidWF4c0FRN2R1bDl5bnI0YU9xWVVQZmIxWkdEcWhSZVlzSG1nc2Q3SlpGTVRhTnc1TWthRmVrMDZmcXBlcklGY3V6a3VUeVlkbWNTQ21INERWYS90bG5EZnVtN1hOYVNNWDA0RGczK09UdmFaV01iQjY5QzZsUElpWFREaTFMWm0iLCJtYWMiOiI5OGNmMDY2YTg1YTdhNjZiZDkyYmJjNWYwYmRkZDc4MjIwNjk5ODBmMWIwNmU2Nzg2MmVjNmFiNWI2YjFmNWNmIiwidGFnIjoiIn0%3D",
-    "crisp-client%2Fsession%2F595850e5-08f3-4ce7-82e8-129ab88f150c": "session_41886dbd-864a-4f3b-bd76-a70ea4aa69e2",
+    "XSRF-TOKEN": "eyJpdiI6InNQakVFbTBSL2NVaGFGNWNJcjJBUFE9PSIsInZhbHVlIjoiNTh5dnYxeTI2SDlBQjRGREw1eEtreVFjM0FOaTJVTnJ3aG85bWl4SG9LdSs2MHROR21OcGo3YkRZTndGVWZyWkV5UU5wY1ZDWFoxVmFxRTJ2akVLenNrYmMzK1doZkpjaDgwR3VwSzlKOW5pSTZINkxCcGp6cUo2RUpOaWlTUG4iLCJtYWMiOiJkNDI0MTVlNzJmMzQzM2ZkM2MyZTA4MjQzZTgwYjg3NTI4Y2ZmNjJmNmExZDhmZDMwOTdiMzZjNmMwN2QzNmU3IiwidGFnIjoiIn0%3D",
+    "mymoneyja_session": "eyJpdiI6ImZPYXM4K0UvdmdBTDZHZnBlOWxxU3c9PSIsInZhbHVlIjoiZ0tNbkdNVUNQMGZYdXZKUGpTQ2c5OTRReVd6bnJKcHI2VjRvME1TZlNWeGJCU0QyWU1lMVp2NEx2d1Y3T3FhcU0xR09QOEprc2VhN1BVRURvaWpBQXJpbldzZzRmd0srN2F3RkVsWm1Ed3NDYUhjWHFiakw4R1FzTkVNbWo3eTAiLCJtYWMiOiI3OTE3MTJmZTUxZDMxOTE3N2JiZTIxNjY0N2ZhM2QwODBmOGJjODNhNWNiZDJjMmViY2ZlYTQ3YjA1MGFiYTBkIiwidGFnIjoiIn0%3D",
+    "crisp-client%2Fsession%2F595850e5-08f3-4ce7-82e8-129ab88f150c": "session_c4ac7265-44dc-4714-8f37-1f9dae4bfa38",
 }
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:93.0) Gecko/20100101 Firefox/93.0",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept": "text/html, application/xhtml+xml",
     "Accept-Language": "en-US,en;q=0.5",
+    "X-Requested-With": "XMLHttpRequest",
+    "X-Inertia": "true",
+    "X-Inertia-Version": "9e2f6c6666c6f388d3c296e25c362a23",
+    "X-XSRF-TOKEN": "eyJpdiI6InQrOUpnZERMR3ZVTHNhWFVZd2hGT1E9PSIsInZhbHVlIjoicDJoa1pIdHRidzJnTlRtcHBaTm83SklpK1ArWGV3ZG1ERU9rdGZXSTBRMWRUQTJHdVBaVE8vL2xWeks3N054MmdhYStaR0VYWWtZODhjLzBQSUhSdnl6OGdkQjhDa2x2ZXFweENFM2MrK0hQdTBkTVBIQ0E5d1Z3WGlkLzFMaWYiLCJtYWMiOiI0ZDAyNGU5OWM2ZTc4NWVmY2E0MjQwOGQ2ZTVkNjc2NWUwMTQwMjNiNzgwMTdhZTBhZGNkZTVkYjI1NmI5ZjQ2IiwidGFnIjoiIn0=",
+    "Referer": "https://mymoneyja.com/login",
     "Connection": "keep-alive",
-    "Upgrade-Insecure-Requests": "1",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "none",
-    "Sec-Fetch-User": "?1",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
     "TE": "trailers",
 }
+email = os.environ.get("EMAIL")
+password = os.environ.get("PASSWORD")
 
-# response = requests.get(
-#     "https://mymoneyja.com/stock/GK", headers=headers, cookies=cookies
-# )
-response = requests.get(
-    "https://mymoneyja.com/market/api", headers=headers, cookies=cookies
+data = '{"email":{email},"password":{password},"remember":true}'
+
+login_response = session.post(
+    "https://mymoneyja.com/login", headers=headers, cookies=cookies, data=data
+)
+
+
+response = session.get(
+    "https://mymoneyja.com/market/api", headers=headers, data=data, cookies=cookies
 )
 
 # get scaped data
-soup = BeautifulSoup(response.content, "html.parser")
-stock_data = json.loads(soup.text)
+stock_data = json.loads(response.content)
 
 company = {}
 # puts each company in a dictionary
 for comp in stock_data:
     company[comp["ticker"]] = comp
 
+
+# response2 = session.get(
+#     "https://mymoneyja.com/stock/GK", headers=headers, data=data, cookies=cookies
+# )
+# soup = BeautifulSoup(response2.content, "html.parser")
+# stock_data = json.loads(soup.text)
+# company["GK"] = stock_data
+
 for key in company:
-    response = requests.get(
-        "https://mymoneyja.com/stock/{key}", headers=headers, cookies=cookies
+    response2 = session.get(
+        "https://mymoneyja.com/stock/{0}".format(key),
+        headers=headers,
+        data=data,
+        cookies=cookies,
     )
-    key_soup = BeautifulSoup(response.content, "html.parser")
+    soup = BeautifulSoup(response2.content, "html.parser")
+    # div_data = soup.find("div", id="app")
+    stock_data = json.loads(soup.text)
+    # stock_data = json.loads(div_data["data-page"])
 
-    # key_stock_data = json.loads(key_soup.text)
-    # company[key]["data"] = key_soup["props"]
-    # company[key]["data"]["volume"] = key_stock_data["props"]["company"]["data"]["volume"]
+    company[key] = stock_data
 
-# companies = stock_data["props"]["companies"]
-print(company["1834"])
+# print(soup)
+print(company["GK"])
