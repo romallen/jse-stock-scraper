a018c8299e83c6ac8258afd7e755823585720431
Romaine Allen <romallen1@gmail.com>
2021-11-03 16:29:18 +0900

    Create dataframe and plot rsi

--- ta.py
+++ ta.py
@@ -10,16 +10,24 @@ client = pymongo.MongoClient(os.environ.get("DB_URL"))
 db = client["jse"]
 col = db["companies"]
 
-items = col.find({"data.ticker": "FESCO"})
+items = col.find({"data.ticker": "FESCO"}, {"pv": 1, "_id": 0})
 stock = []
-for x in items:
-    stock = x["pv"]
+# for x in items:
+# stock = x["pv"]
+# print(x)
 
-# data=stock, columns=("Date", "Open", "High", "Low", "Close", "Volume")
-df = pd.DataFrame()
+df = pd.DataFrame(
+    data=list(items)[0]["pv"],
+    columns=("unix_date", "open", "high", "low", "close", "volume"),
+)
+df["date"] = pd.to_datetime(df["unix_date"], unit="ms")
 
+# df["rsi"] = ta.rsi(close=df.close, length=10)
+df["rsi"] = df.ta.rsi()
+
+plt.plot(df.date, df.rsi)
 print(df)
-
+plt.show()
 # ta.mom
 # df["rsi"] = ta.momentum.RSIIndicator(df[4], window=14).rsi()
 # plt.plot(df[0], df["rsi"])
