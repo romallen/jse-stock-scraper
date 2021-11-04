00fc41092c069e175cf3ebd8ef2a4724a7a20442
Romaine Allen <romallen1@gmail.com>
2021-11-04 20:25:39 +0900

    Add chart switching

--- app.py
+++ app.py
@@ -35,8 +35,6 @@ for tick in get_tickers:
 # df = create_df("FESCO")
 
 
-# fig0 = px.line(df, x=df.date, y=df.close)
-
 app.layout = html.Div(
     [
         html.H1(children="JAMAICA STOCK EXCHANGE CHARTS"),
@@ -47,26 +45,59 @@ app.layout = html.Div(
             options=company_list,
             value="138SL",
         ),
+        html.Label("Select type of graph"),
+        dcc.RadioItems(
+            id="chart-type",
+            options=[
+                {"label": "Line", "value": "line"},
+                {"label": "Candle Stick", "value": "candle"},
+                {"label": "OHLC", "value": "ohlc"},
+            ],
+            value="line",
+        ),
         dcc.Graph(id="stock-chart"),
     ]
 )
 
 
-@app.callback(Output("stock-chart", "figure"), Input("drop-selection", "value"))
-def update_figure(ticker):
+@app.callback(
+    Output("stock-chart", "figure"),
+    Input("drop-selection", "value"),
+    Input("chart-type", "value"),
+)
+def update_ticker(ticker, chart_type):
     items = col.find({"data.ticker": ticker}, {"pv": 1, "_id": 0})
     df = pd.DataFrame(
         data=list(items)[0]["pv"],
         columns=("unix_date", "open", "high", "low", "close", "volume"),
     )
     df["date"] = pd.to_datetime(df["unix_date"], unit="ms")
-    df.set_index("unix_date", inplace=True)
+    # df.set_index("unix_date", inplace=True)
 
-    return go.Figure(
-        data=[
-            go.Ohlc(x=df.date, open=df.open, high=df.high, low=df.low, close=df.close)
-        ]
-    )
+    if chart_type == "line":
+        return go.Figure(
+            data=[
+                go.Scatter(
+                    x=df.date, y=df.close, mode="lines+markers", name="lines+markers"
+                )
+            ]
+        )
+    elif chart_type == "candle":
+        return go.Figure(
+            data=[
+                go.Candlestick(
+                    x=df.date, open=df.open, high=df.high, low=df.low, close=df.close
+                )
+            ]
+        )
+    elif chart_type == "ohlc":
+        return go.Figure(
+            data=[
+                go.Ohlc(
+                    x=df.date, open=df.open, high=df.high, low=df.low, close=df.close
+                )
+            ]
+        )
 
 
 if __name__ == "__main__":
