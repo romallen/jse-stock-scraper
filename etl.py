019502518efb9fa909c97d0b550a98d001bf8bc0
Romaine Allen <romallen1@gmail.com>
2021-12-17 18:57:09 +0900

    Deleted unneccessary code and files

--- etl.py
+++ etl.py
@@ -14,24 +14,26 @@ s3 = boto3.resource('s3')
 
 session = requests.Session()
 cookies = {
-   'XSRF-TOKEN': 'eyJpdiI6Ik1NWFhIS1dHcGtHdzFreDRXVmxhVXc9PSIsInZhbHVlIjoicmxoTGFEMnZvTjA2STQrRy9sYm1sS1ErU0ptMkxMQThKM2E0YXV4Y0VITkRYczlycHM3NHFSOHl4czFmYjNRVzFCTjNxSjRqYzlGVXUraW1QSjdNdWxubU96UzFvKzVHeFdtRmJyZmpyUG40MjUzRWlIQmZxNG0vZ0I0bUI4K1UiLCJtYWMiOiIyMzNmZWZiZTE4MGM4NjM1NGU1ZWU1MTZlYmU0ZmIzMzhhZmYyMDliMTU2NDJlNzA0NjA1MmMyY2U0ZTQyNzYzIiwidGFnIjoiIn0%3D',
-    'mymoneyja_session': 'eyJpdiI6Ijg3UWdTWDAyMmRPVlBUL0g0K24zcHc9PSIsInZhbHVlIjoiVDl5bEVqcVN5TW1qdFEyY3p5SFVLaEIzR09KRGcydkR0R0FTTSswcmpsak94SnFnbEJaM0I0d3NsMXVIWDJ1NXFIa3gxSm4rbWNmd1h2b1R5UmdXdDJmVTZXTEdzNEhQZmp0UnQ5UjllODJkcW9hdy9iQWdyM1NlVFo3U0Z1djMiLCJtYWMiOiJiNTYxMDZkYmNlZTNjZWM4MjdmOTY2NThlMGE5YzdkNjM5MDEzYjBlNjhmOTIwMTAzZDMyMjU4NGI1NjhjMGQyIiwidGFnIjoiIn0%3D',
-    'crisp-client%2Fsession%2F595850e5-08f3-4ce7-82e8-129ab88f150c': 'session_0b59d118-8d04-4382-af1f-8f2d997f7e2e',
-    'remember_web_3dc7a913ef5fd4b890ecabe3487085573e16cf82': 'eyJpdiI6IjJoZmpYWjFYOENjQjQ1Lyt2TVpNSEE9PSIsInZhbHVlIjoiRkRCSkJwcXpvZG12MTNiWmhZOTU0cmdqQU9rUGgrQUw5V3dkU01HTENlK3lYdmJHYkVibldxWDJENmNlK2w1QzJHSStTdzdHVzhYYXE2UFUwdnEwRTZGVzQwUUxrKzErMHoyRTdreC9ra3NDL29lNkVSaWRVanZIVFNBTmZwamJOU2RmRGJtbG1FYWdoVnJ6M0RKV3pnR2s1SkhJUW9mQUtJQmdFRSsxZWdISWNHNnRnNFlLcVkwbnJwTXNYY2xCZ1h3ak9MeDV4MGZPUHVJUmJwNGlDYng5YmNOekxlUFcwTElLdEFKWjJkdz0iLCJtYWMiOiJiYjRjYjdkNWI2MzQ0NTY0Y2Q1Y2FkZDAyYTc3NGVjNDMxZWZjNzZjZjVkZGQwMzllODBkYjUzODE1MmRhOTZjIiwidGFnIjoiIn0%3D'
+     'remember_web_3dc7a913ef5fd4b890ecabe3487085573e16cf82': 'eyJpdiI6IjJoZmpYWjFYOENjQjQ1Lyt2TVpNSEE9PSIsInZhbHVlIjoiRkRCSkJwcXpvZG12MTNiWmhZOTU0cmdqQU9rUGgrQUw5V3dkU01HTENlK3lYdmJHYkVibldxWDJENmNlK2w1QzJHSStTdzdHVzhYYXE2UFUwdnEwRTZGVzQwUUxrKzErMHoyRTdreC9ra3NDL29lNkVSaWRVanZIVFNBTmZwamJOU2RmRGJtbG1FYWdoVnJ6M0RKV3pnR2s1SkhJUW9mQUtJQmdFRSsxZWdISWNHNnRnNFlLcVkwbnJwTXNYY2xCZ1h3ak9MeDV4MGZPUHVJUmJwNGlDYng5YmNOekxlUFcwTElLdEFKWjJkdz0iLCJtYWMiOiJiYjRjYjdkNWI2MzQ0NTY0Y2Q1Y2FkZDAyYTc3NGVjNDMxZWZjNzZjZjVkZGQwMzllODBkYjUzODE1MmRhOTZjIiwidGFnIjoiIn0%3D',
+    'XSRF-TOKEN': 'eyJpdiI6InQrV1N3bDBzS2ZxWUJHNEJXRFhSc2c9PSIsInZhbHVlIjoiYkxHN3VKcWovS2M3RmdSQmZIMS95N0E5TTRoMDRHU2M1dTArYTdPSTdkRWxJVGNwRjZ5R3BrSmtGRnhwSThlSHZXQkhyTDBRL28rYVJ3ZFVWMFhNdWdlalMxbjNqT2pEQnc1KzRNM0x1NHdxOWRRZEp6bVNxSUVpZ1hNNDBhalIiLCJtYWMiOiI1NDEwOTc2MTEwMGE0MjkxZDg5N2UyZDQzYWIxNTgwMWYzOWQwNWFjZjMxMWFiMDg3NWIyMjBiMzMwMjkyM2VlIiwidGFnIjoiIn0%3D',
+    'mymoneyja_session': 'eyJpdiI6ImgyTXUyUlRwUXpBL2xCbmJjTGRPTlE9PSIsInZhbHVlIjoia2FId09SemV4RGt2SmpKZ2lPVURCR0ZDRnRVclAvSW5YemprMk1FNVdheUdJRjI0RDlNTkVhTXBwOHhmZ0VGcndLMDN3MUhmZnh2Wkp0ZHhVL3Vta296cUIvR0QxR3hyR2ZyYkMwZXQxOVBueEM3UE9jdFdjYkN2dHlQSDNUYy8iLCJtYWMiOiJlODQ0MTE5NGY0MWMyYzU4ZjU0ZmIyM2NlZWRlMWUxZTQ4ODU5NmExYzUyODNkY2VjOTIwOTc5ZWMwZDIwMjc3IiwidGFnIjoiIn0%3D',
+    'crisp-client%2Fsession%2F595850e5-08f3-4ce7-82e8-129ab88f150c': 'session_e4c573dc-144c-440f-bb5e-2672ee260f07',
+
     }
 
 
 session.headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:95.0) Gecko/20100101 Firefox/95.0',
+     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:95.0) Gecko/20100101 Firefox/95.0',
     'Accept': 'text/html, application/xhtml+xml',
     'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate, br',
     'X-Requested-With': 'XMLHttpRequest',
     'X-Inertia': 'true',
-    'X-Inertia-Version': 'eaab6f8db0b109568ae5e7468dbd534a',
-    'X-XSRF-TOKEN': 'eyJpdiI6IitnM3BydE1nMk9ZeUh5U2g2U29RaFE9PSIsInZhbHVlIjoiQVBoUWRnem9HU1g3S1JMK3ZSNWZ6V0greEN2TVlFRmF1YVJWZW9NcFVWd1RTY3hKVzBKUVVRSnU1ZlZnT0Y5bFpRQy9IU1BGci91eG1ZM0dOcjd5Qk1oWG0rMzdsckFQR29oeXpHSlo1SjJqcE9hSjJzOUtmZ0pkRzRFb0NHSkIiLCJtYWMiOiI2MTI2MzNiNGMzNjNlODUwMjdiYWM1NGY0ZWI1ZDMyMDliNjg2NTEyNTEyNDMxYTViN2IyMTc1M2UzOWE5ZTZhIiwidGFnIjoiIn0=',
-    'Referer': 'https://mymoneyja.com/login',
+    'X-Inertia-Version': '0ef7f1ebf4673ce29938a6c1c794971d',
+    'Content-Type': 'application/json',
+    'X-XSRF-TOKEN': 'eyJpdiI6InQrV1N3bDBzS2ZxWUJHNEJXRFhSc2c9PSIsInZhbHVlIjoiYkxHN3VKcWovS2M3RmdSQmZIMS95N0E5TTRoMDRHU2M1dTArYTdPSTdkRWxJVGNwRjZ5R3BrSmtGRnhwSThlSHZXQkhyTDBRL28rYVJ3ZFVWMFhNdWdlalMxbjNqT2pEQnc1KzRNM0x1NHdxOWRRZEp6bVNxSUVpZ1hNNDBhalIiLCJtYWMiOiI1NDEwOTc2MTEwMGE0MjkxZDg5N2UyZDQzYWIxNTgwMWYzOWQwNWFjZjMxMWFiMDg3NWIyMjBiMzMwMjkyM2VlIiwidGFnIjoiIn0=',
     'Connection': 'keep-alive',
+    'Referer': 'https://mymoneyja.com/market',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
@@ -51,18 +53,9 @@ def login():
         cookies=cookies,
         headers=session.headers,
     )
-    # print(login_response.headers)
-    # ession.cookies = session.cookies.get_dict()
+
     print(login_response.headers)
 
-    # used to get list of company tickers
-    # response = session.get("https://mymoneyja.com/stock/138SL", data=data)
-    # soup = BeautifulSoup(response, "html.parser")
-    # print(response.text)
-
-    # print(response.content)
-    # get scaped data
-    # return json.loads(response.content)
 
 
 # login()
@@ -148,8 +141,3 @@ coll = db["companies"]
 y = coll.insert_many(documents)
 print(y)
 
-
-
-# x = coll.insert_one(documents[0])
-# x = col.insert_many(documents)
-# print(x)
