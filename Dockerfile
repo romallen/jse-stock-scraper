0afda899fbbd309e001c974e2e4da7a33e525a71
Romaine Allen <romallen1@gmail.com>
2022-06-15 11:45:42 +0900

    Refactor Headers and beautiful soup code

--- Dockerfile
+++ Dockerfile
@@ -1,50 +1,27 @@
-#For more information, please refer to https://aka.ms/vscode-docker-python
-FROM public.ecr.aws/lambda/python:3.8
+FROM python:3.9
 
-# Copy function code
-COPY etl-sel.py ${LAMBDA_TASK_ROOT}
-# Keeps Python from generating .pyc files in the container
-ENV PYTHONDONTWRITEBYTECODE=1
+ADD etl-sel.py .
 
-# Turns off buffering for easier container logging
-ENV PYTHONUNBUFFERED=1
+COPY requirements.txt .
 
-# Install pip requirements
-COPY requirements.txt ${LAMBDA_TASK_ROOT}
-RUN --mount=type=cache,target=/root/.cache/pip python3.6 -m pip install --upgrade pip
-RUN --mount=type=cache,target=/root/.cache/pip python3.6 -m pip install -r requirements.txt
+# COPY .venv .
 
+# install google chrome
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+RUN sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+RUN apt-get -y update
+RUN apt-get install -y chromium
 
-# RUN python -m pip install -r requirements.txt
+# install chromedriver
+RUN apt-get install -yqq unzip
+RUN wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
 
-RUN mkdir driver
-COPY driver driver/
+# set display port to avoid crash
+# ENV DISPLAY=:99
 
+# install selenium
+RUN pip install -r requirements.txt
+# RUN python -m pip install selenium
 
-# RUN chmod 755 /driver/chromedriver
-
-
-# During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
-CMD ["etl-sel.scraper"]
-
-   
-# FROM public.ecr.aws/lambda/python@sha256:258a25364fd462f6c6c387cf1bea4a79fa1a8b09aee578aef2c7c50b5efca928 as build
-# RUN yum install -y unzip && \
-#     curl -Lo "/tmp/chromedriver.zip" "https://chromedriver.storage.googleapis.com/99.0.4844.51/chromedriver_linux64.zip" && \
-#     curl -Lo "/tmp/chrome-linux.zip" "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F961656%2Fchrome-linux.zip?alt=media" && \
-#     unzip /tmp/chromedriver.zip -d /opt/ && \
-#     unzip /tmp/chrome-linux.zip -d /opt/
-
-# FROM public.ecr.aws/lambda/python@sha256:258a25364fd462f6c6c387cf1bea4a79fa1a8b09aee578aef2c7c50b5efca928
-# RUN yum install atk cups-libs gtk3 libXcomposite alsa-lib \
-#     libXcursor libXdamage libXext libXi libXrandr libXScrnSaver \
-#     libXtst pango at-spi2-atk libXt xorg-x11-server-Xvfb \
-#     xorg-x11-xauth dbus-glib dbus-glib-devel -y
-
-# COPY requirements.txt .
-# RUN python -m pip install -r requirements.txt
-
-# COPY --from=build /opt/chrome-linux /opt/chrome
-# COPY --from=build /opt/chromedriver /opt/
-# COPY etl-sel.py ./
-# CMD [ "etl-sel.scraper" ]
+CMD [ "python", "./etl-sel.py" ]
--- etl-sel.py
+++ etl-sel.py
@@ -1,50 +1,44 @@
-
-from asyncio.log import logger
-from decimal import Decimal
-import shutil
 import requests
 from bs4 import BeautifulSoup
 import json
 from dotenv import load_dotenv
 import os
-from requests.sessions import Request
 import pymongo
 import boto3
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from seleniumwire import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.utils import ChromeType
 import time
 from concurrent.futures import ThreadPoolExecutor
-from tempfile import mkdtemp
+
 
 
 load_dotenv()
 s3 = boto3.resource('s3')
-dynamodb = boto3.resource('dynamodb')
+# dynamodb = boto3.resource('dynamodb')
 session = requests.Session()
 
 session.headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:95.0) Gecko/20100101 Firefox/95.0',
-        'Accept': 'text/html, application/xhtml+xml',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'X-Requested-With': 'XMLHttpRequest',
-        'X-Inertia': 'true',
-        'X-Inertia-Version': '0ef7f1ebf4673ce29938a6c1c794971d',
-        'Content-Type': 'application/json',
-        'X-XSRF-TOKEN': 'eyJpdiI6InQrV1N3bDBzS2ZxWUJHNEJXRFhSc2c9PSIsInZhbHVlIjoiYkxHN3VKcWovS2M3RmdSQmZIMS95N0E5TTRoMDRHU2M1dTArYTdPSTdkRWxJVGNwRjZ5R3BrSmtGRnhwSThlSHZXQkhyTDBRL28rYVJ3ZFVWMFhNdWdlalMxbjNqT2pEQnc1KzRNM0x1NHdxOWRRZEp6bVNxSUVpZ1hNNDBhalIiLCJtYWMiOiI1NDEwOTc2MTEwMGE0MjkxZDg5N2UyZDQzYWIxNTgwMWYzOWQwNWFjZjMxMWFiMDg3NWIyMjBiMzMwMjkyM2VlIiwidGFnIjoiIn0=',
-        'Connection': 'keep-alive',
-        'Referer': 'https://mymoneyja.com/market',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-origin',
-        'TE': 'trailers',
+       
+        'authority': 'mymoneyja.com',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language': 'en-US,en;q=0.9,ja;q=0.8,fr;q=0.7',
+        'dnt': '1',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36',
     }
 
 def initailize_scraper():
+
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
@@ -54,9 +48,11 @@ def initailize_scraper():
     options.add_argument("--single-process")
   
     options.add_argument(
-        '"user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"')
-
+        '"user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"')
+    
     page_to_scrape= webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    
+    page_to_scrape.implicitly_wait(10)
     page_to_scrape.get("https://mymoneyja.com/login")
     
     email = page_to_scrape.find_element(By.NAME, "email")
@@ -66,35 +62,36 @@ def initailize_scraper():
 
     page_to_scrape.find_element(By.CSS_SELECTOR, "body > div > div > form > div:nth-child(4) > button").click()
     time.sleep(3)
-    page_to_scrape.find_element(By.CSS_SELECTOR, "a.w-full:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3)").click()
-    time.sleep(3)
+    # page_to_scrape.find_element(By.CSS_SELECTOR, "a.w-full:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3)").click()
+    # time.sleep(3)
     selCookies = page_to_scrape.get_cookies()
-   
     #store the headers in the session
-    for request in page_to_scrape.requests:
-        if request.headers['x-xsrf-token']:
-            del session.headers['Referer'] 
-            del session.headers['X-XSRF-TOKEN'] 
-            del session.headers['User-Agent'] 
-            del session.headers['X-Inertia-Version'] 
-            session.headers['Referer'] = request.headers['referer']
-            session.headers['X-XSRF-TOKEN'] = request.headers['x-xsrf-token']
-            session.headers['User-Agent'] = request.headers['user-agent']
-            session.headers['X-Inertia-Version'] = request.headers['x-inertia-version']
+    # for request in page_to_scrape.requests:
+    #     if request.headers['x-xsrf-token']:
+    #         print("starting scraper",  request)
+            # session.headers.update(request.headers)
+            # del session.headers['Referer'] 
+            # del session.headers['X-XSRF-TOKEN'] 
+            # del session.headers['User-Agent'] 
+            # del session.headers['X-Inertia-Version'] 
+            # session.headers['Referer'] = request.headers['referer']
+            # session.headers['X-XSRF-TOKEN'] = request.headers['x-xsrf-token']
+            # session.headers['User-Agent'] = request.headers['user-agent']
+            # session.headers['X-Inertia-Version'] = request.headers['x-inertia-version']
             
             
     #store the cookies in the session
     for cookie in selCookies:
         session.cookies[cookie["name"]] = cookie["value"]
-
     time.sleep(5)
 
 
 def gen_company_list():
     response = session.get("https://mymoneyja.com/stock/138SL")
-
+    comp_list_soup = BeautifulSoup(response.content, "html.parser")
+    comp_data =comp_list_soup.find("div", {"id": "app"})
     # get scaped data for tickers
-    stock_data = json.loads(response.content)
+    stock_data = json.loads(comp_data.attrs["data-page"])
 
 
     # saves the companies tickers in a dictionary
@@ -115,7 +112,10 @@ def get_data(company):
        
     )
     soup = BeautifulSoup(response2.content, "html.parser")
-    stock_data = json.loads(soup.text)
+    comp_data = soup.find("div", {"id": "app"})
+    # get scaped data for tickers
+    stock_data = json.loads(comp_data.attrs["data-page"])
+    # stock_data = json.loads(soup.text)
     key = {}
    
     key["name"] = stock_data["props"]["company"]["name"]
@@ -170,15 +170,15 @@ def store_mongo():
     print(z)
     
     
-def store_dynamo(companyDocuments):
-    table = dynamodb.Table('jse')
+# def store_dynamo(companyDocuments):
+#     table = dynamodb.Table('jse')
     
-    companies =json.loads(json.dumps(companyDocuments), parse_float=Decimal)
+#     companies =json.loads(json.dumps(companyDocuments), parse_float=Decimal)
 
-    for doc in companies:
-        ticker = doc["ticker"]
-        print("Storing: ", ticker) 
-        table.put_item(Item=doc)
+#     for doc in companies:
+#         ticker = doc["ticker"]
+#         print("Storing: ", ticker) 
+#         table.put_item(Item=doc)
   
     
 
